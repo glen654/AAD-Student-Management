ad516c5763197957e81a03265f51f68abd2c24ef
Glen Alloy <glenalloy8@gmail.com>
2024-07-12 11:51:48 +0530

    created StudentDto class using lombok

--- src/main/java/lk/ijse/aadstudentmanagement/controller/StudentController.java
+++ src/main/java/lk/ijse/aadstudentmanagement/controller/StudentController.java
@@ -1,17 +1,6 @@
 package lk.ijse.aadstudentmanagement.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class StudentController implements Serializable {
-    private String id;
-    private String name;
-    private String city;
-    private String email;
-    private String level;
+public class StudentController{
+
 }
--- src/main/java/lk/ijse/aadstudentmanagement/dto/StudentDto.java
+++ src/main/java/lk/ijse/aadstudentmanagement/dto/StudentDto.java
@@ -1,4 +1,17 @@
 package lk.ijse.aadstudentmanagement.dto;
 
-public class StudentDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StudentDto implements Serializable {
+    private String id;
+    private String name;
+    private String city;
+    private String email;
+    private String level;
 }
