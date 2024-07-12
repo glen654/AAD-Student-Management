2c66bdffd30fa2af0b54a71db43612f45f7b7748
Glen Alloy <glenalloy8@gmail.com>
2024-07-12 12:01:45 +0530

    created static mysql query strings

--- src/main/java/lk/ijse/aadstudentmanagement/controller/StudentController.java
+++ src/main/java/lk/ijse/aadstudentmanagement/controller/StudentController.java
@@ -8,26 +8,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 @WebServlet(urlPatterns = "")
 public class StudentController extends HttpServlet {
+    Connection connection;
+    static String SAVE_STUDENT = "INSERT INTO student VALUES(?,?,?,?,?)";
+    static String GET_STUDENT = "SELECT * FROM student WHERE id = ?";
+
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        //Todo: Save Student
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        //Todo: Get Student Details
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+        //Todo: Update Student
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+        //Todo: Delete Student
     }
 }
