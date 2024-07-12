776943c7d9ca9bf4a4eba4fcd08bf75b955fae9b
Glen Alloy <glenalloy8@gmail.com>
2024-07-12 11:55:43 +0530

    made the student controller a servlet class and override the do methods

--- src/main/java/lk/ijse/aadstudentmanagement/controller/StudentController.java
+++ src/main/java/lk/ijse/aadstudentmanagement/controller/StudentController.java
@@ -1,6 +1,33 @@
 package lk.ijse.aadstudentmanagement.controller;
 
 
-public class StudentController{
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "")
+public class StudentController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
 }
