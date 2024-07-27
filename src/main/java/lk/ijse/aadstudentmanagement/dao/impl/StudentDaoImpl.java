38e701ea324a26bde13bd9a1867b86f392c54965
Glen Alloy <glenalloy8@gmail.com>
2024-07-27 11:34:50 +0530

    implemented sql quries in the student dao

--- src/main/java/lk/ijse/aadstudentmanagement/dao/impl/StudentDaoImpl.java
+++ src/main/java/lk/ijse/aadstudentmanagement/dao/impl/StudentDaoImpl.java
@@ -7,6 +7,10 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class StudentDaoImpl implements StudentDao {
+    static String SAVE_STUDENT = "INSERT INTO student (id,name,city,email,level) VALUES (?,?,?,?,?)";
+    static String GET_STUDENT = "SELECT * FROM student WHERE id=?";
+    static String UPDATE_STUDENT = "UPDATE student SET name=?,city=?,email=?,level=? WHERE id=?";
+    static String DELETE_STUDENT = "DELETE FROM student WHERE id=?";
     @Override
     public StudentDto getStudent(String studentId, Connection connection) throws SQLException {
         return null;
