4669a721349f13c4c6bc48ab583c736e4a3b552e
Glen Alloy <glenalloy8@gmail.com>
2024-07-27 11:14:29 +0530

    implementd generate id class

--- src/main/java/lk/ijse/aadstudentmanagement/util/UtilProcess.java
+++ src/main/java/lk/ijse/aadstudentmanagement/util/UtilProcess.java
@@ -1,4 +1,9 @@
 package lk.ijse.aadstudentmanagement.util;
 
+import java.util.UUID;
+
 public class UtilProcess {
+    public static String generateId(){
+        return UUID.randomUUID().toString();
+    }
 }
