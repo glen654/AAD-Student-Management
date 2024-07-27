f1733d7a858bfcecb5697ed55fca9f652a971b2e
Glen Alloy <glenalloy8@gmail.com>
2024-07-27 11:28:34 +0530

    created studentDao methods and implemented the methods in studentDaoImpl

--- src/main/java/lk/ijse/aadstudentmanagement/dao/StudentDao.java
+++ src/main/java/lk/ijse/aadstudentmanagement/dao/StudentDao.java
@@ -1,4 +1,16 @@
 package lk.ijse.aadstudentmanagement.dao;
 
-public interface StudentDao {
+import lk.ijse.aadstudentmanagement.dto.StudentDto;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface StudentDao{
+    StudentDto getStudent(String studentId, Connection connection) throws SQLException;
+
+    boolean saveStudent(StudentDto studentDto, Connection connection);
+
+    boolean deleteStudent(String studentId,Connection connection);
+
+    boolean updateStudent(String studentId,StudentDto studentDto,Connection connection);
 }
--- src/main/java/lk/ijse/aadstudentmanagement/dao/impl/StudentDaoImpl.java
+++ src/main/java/lk/ijse/aadstudentmanagement/dao/impl/StudentDaoImpl.java
@@ -1,4 +1,29 @@
 package lk.ijse.aadstudentmanagement.dao.impl;
 
-public class StudentDaoImpl {
+import lk.ijse.aadstudentmanagement.dao.StudentDao;
+import lk.ijse.aadstudentmanagement.dto.StudentDto;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class StudentDaoImpl implements StudentDao {
+    @Override
+    public StudentDto getStudent(String studentId, Connection connection) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean saveStudent(StudentDto studentDto, Connection connection) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteStudent(String studentId, Connection connection) {
+        return false;
+    }
+
+    @Override
+    public boolean updateStudent(String studentId, StudentDto studentDto, Connection connection) {
+        return false;
+    }
 }
